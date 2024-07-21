60552c6a634c21df53e6059e15fbd507ddbdacb0
Mulc1ber <dmo@mail.ru>
2024-07-21 13:07:01 +0300

    commit without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 // Начало Императивного стиля
 const currentDate = new Date().getFullYear();
 // Конец Императивного стиля
 
-// Начало Декларативного стиля
+// Начало Императивного стиля
 export const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    This is{' '}
-                    <b>
-                        <u>Declarative style</u>
-                    </b>
-                    . In file <code>src/App.js</code> component App.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <p>{currentDate} год</p>
-            </header>
-        </div>
+    return React.createElement(
+        'div',
+        { className: 'App' },
+        React.createElement(
+            'header',
+            { className: 'App-header' },
+            React.createElement('img', {
+                src: logo,
+                className: 'App-logo',
+                alt: 'logo',
+            }),
+            React.createElement(
+                'p',
+                null,
+                'This is ',
+                React.createElement('b', null, React.createElement('u', null, 'Imperative style.')),
+                ' In file ',
+                React.createElement('code', null, 'src/App.js'),
+                ' component App.',
+            ),
+            React.createElement(
+                'a',
+                {
+                    className: 'App-link',
+                    href: 'https://reactjs.org',
+                    target: '_blank',
+                    rel: 'noopener noreferrer',
+                },
+                'Learn React',
+            ),
+            React.createElement('p', null, currentDate, ' год'),
+        ),
     );
 };
-// Конец Декларативного стиля
+// Конец Императивного стиля

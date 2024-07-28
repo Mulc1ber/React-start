1d862522f154f131152d004b6b816c01649432b4
Mulc1ber <dmo@mail.ru>
2024-07-28 21:39:56 +0300

    complite task1 lesson - Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-// Начало Императивного стиля
-const currentDate = new Date().getFullYear();
-// Конец Императивного стиля
-
-// Начало Императивного стиля
 export const App = () => {
-    return React.createElement(
-        'div',
-        { className: 'App' },
-        React.createElement(
-            'header',
-            { className: 'App-header' },
-            React.createElement('img', {
-                src: logo,
-                className: 'App-logo',
-                alt: 'logo',
-            }),
-            React.createElement(
-                'p',
-                null,
-                'This is ',
-                React.createElement('b', null, React.createElement('u', null, 'Imperative style.')),
-                ' In file ',
-                React.createElement('code', null, 'src/App.js'),
-                ' component App.',
-            ),
-            React.createElement(
-                'a',
-                {
-                    className: 'App-link',
-                    href: 'https://reactjs.org',
-                    target: '_blank',
-                    rel: 'noopener noreferrer',
-                },
-                'Learn React',
-            ),
-            React.createElement('p', null, currentDate, ' год'),
-        ),
+    const [value, setValue] = useState();
+    const [list, setList] = useState([]);
+    const [error, setError] = useState();
+    const [isValueVaild, setIsValueVaild] = useState(false);
+
+    function onInputButtonClick() {
+        const promptValue = prompt('Введите значение');
+        if (promptValue === null || promptValue.trim().length === 0) {
+            setError('Введено пустое значение или отменено');
+            setIsValueVaild(false);
+        } else if (promptValue.trim().length < 3) {
+            setError('Введенное значение должно содержать минимум 3 символа');
+            setIsValueVaild(false);
+        } else {
+            setValue(promptValue);
+            setError('');
+            setIsValueVaild(true);
+        }
+    }
+
+    function onAddButtonClick() {
+        if (value !== '') {
+            const updatedList = [...list, { id: Date.now(), value }];
+            setList(updatedList);
+            setValue('');
+            setError('');
+            setIsValueVaild(false);
+        }
+    }
+
+    return (
+        <div className={styles['app']}>
+            <h1 className={styles['page-heading']}>Ввод значения</h1>
+            <p className={styles['no-margin-text']}>
+                Текущее значение <code>value</code>: "
+                <output className={styles['current-value']}>{value}</output>"
+            </p>
+
+            {error !== '' && <div className={styles['error']}>{error}</div>}
+
+            <div className={styles['buttons-container']}>
+                <button className={styles['button']} onClick={onInputButtonClick}>
+                    Ввести новое
+                </button>
+                <button
+                    className={styles['button']}
+                    disabled={!isValueVaild}
+                    onClick={onAddButtonClick}
+                >
+                    Добавить в список
+                </button>
+            </div>
+            <div className={styles['list-container']}>
+                <h2 className={styles['list-heading']}>Список:</h2>
+
+                {list.length === 0 && (
+                    <p className={styles['no-margin-tex']}>Нет добавленных элементов</p>
+                )}
+
+                <ul className={styles['list']}>
+                    {list.map(({ id, value }) => {
+                        const formatterDate = new Date(id).toLocaleString('ru-RU');
+
+                        return (
+                            <li className={styles['list-item']} key={id}>
+                                {value} - {formatterDate}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
     );
 };
-// Конец Императивного стиля
